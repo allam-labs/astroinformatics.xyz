14898ef548a1d2d78ee86535b229ea4574a2846d
Tarek Allam <t.allam.jr@gmail.com>
2020-02-12 13:58:33 +0000

    Script to automatically deploy modified site
    	new file:   deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$CONDA_DEFAULT_ENV" == "astroxyz" ]; then
+
+    cd docs;
+    make html;
+    cd _build/html;
+    git add --all;
+    git commit -m "Publishing to gh-pages";
+    git push --all
+
+else
+
+    echo "CONDA_DEFAULT_ENV != astroxyz \n\n
+            Run 'conda activate astroxyz'"
